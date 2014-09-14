360ebbb8160c540514a38f9a4cbcb4eb29e1d66a
Lokesh <lokeshlodha@gmail.com>
2014-09-14 09:53:38 +0530

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
 group :development, :test do
 # Use mysql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907140500) do
+ActiveRecord::Schema.define(version: 20140913140106) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20140907140500) do
   create_table "tweets", force: true do |t|
     t.string   "status"
     t.integer  "zombie_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
